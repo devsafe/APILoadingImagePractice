e6e69bd06d1fda1b950ed5b7f88f37317cc6478a
Rafinad <rafinad@outlook.com>
2021-05-11 23:51:45 +0300

    Initial Commit

---
+++ APILoadingImagePractice/APILoadingImagePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APILoadingImagePracticeApp.swift
+//  APILoadingImagePractice
+//
+//  Created by Сахар Кубический on 11.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct APILoadingImagePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
